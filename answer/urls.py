5341e77268211d111038f6337d05a03ba5556234
tomioka-k <tomioka-k@tajima.co.jp>
2021-08-31 01:29:26 +0900

    create question detail and edit view #10

--- answer/urls.py
+++ answer/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import IndexView, CreateView
+from .views import IndexView, QuestionCreateView, QuestionDetailView, QuestionEditView
 
 app_name = 'answer'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('create/', CreateView.as_view(), name='create'),
-
+    path('create/', QuestionCreateView.as_view(), name='create'),
+    path('<int:pk>/', QuestionDetailView, name='detail'),
+    path('<int:pk>/edit/', QuestionEditView.as_view(), name='edit'),
 ]
