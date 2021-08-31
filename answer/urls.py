46b7f503559eb0aed6a9e2fd4a0f850373207005
tomioka-k <tomioka-k@tajima.co.jp>
2021-09-01 01:50:05 +0900

    create reply delete and edit view #12

--- answer/urls.py
+++ answer/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import IndexView, QuestionCreateView, QuestionDetailView, QuestionEditView
+from .views import IndexView, QuestionCreateView, QuestionDetailView, QuestionEditView, delete_question, delete_reply, ReplyEditView
 
 app_name = 'answer'
 urlpatterns = [
@@ -8,4 +8,9 @@ urlpatterns = [
     path('create/', QuestionCreateView.as_view(), name='create'),
     path('<int:pk>/', QuestionDetailView, name='detail'),
     path('<int:pk>/edit/', QuestionEditView.as_view(), name='edit'),
+    path('<int:pk>/delete/', delete_question, name='delete'),
+    path('<int:question_id>/reply/<int:pk>/delete',
+         delete_reply, name='delete-reply'),
+    path('<int:question_id>/reply/<int:pk>/edit/',
+         ReplyEditView, name='edit-reply'),
 ]
